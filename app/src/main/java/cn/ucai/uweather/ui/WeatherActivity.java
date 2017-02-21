9a32d20a085b0a9f579d47b14a7526e69974cc05
liuran <1227228155@qq.com>
2017-02-21 22:43:21 +0800

    完成 天气界面的布局

---
+++ app/src/main/java/cn/ucai/uweather/ui/WeatherActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.uweather.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ucai.uweather.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
