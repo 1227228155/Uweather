cc73dec328c3247fc4941b84fa1314b0cd2da94e
liuran <1227228155@qq.com>
2017-03-07 14:23:42 +0800

    修改布局

--- app/src/main/java/cn/ucai/uweather/ui/MainActivity.java
+++ app/src/main/java/cn/ucai/uweather/ui/MainActivity.java
@@ -6,26 +6,33 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
 import butterknife.ButterKnife;
 import cn.ucai.uweather.R;
 
 public class MainActivity extends FragmentActivity {
     ChooseAreaFragment chooseAreaFragment;
-
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        gotoActivity();
+    }
+
+    private void gotoActivity() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (preferences.getString("bing_pic",null)!=null){
+        if (preferences.getString("weather_id",null)!=null){
+            Log.e(TAG, preferences.getString("weather_id", null));
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             this.finish();
         }else {
-            Amap();
+             Amap();
+
         }
     }
 
